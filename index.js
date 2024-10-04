9e2fa14d023bbcae2d528ef0b8827a9f57849d64
vikasdev66 <vikasmishra.webdev@gmail.com>
2024-10-05 01:28:04 +0530

    1.0.0

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const Product = require("./models/product.model.js");
+const productRoutes = require("./routes/product.route.js");
+const app = express();
+
+// app.use(express.json()); is a middleware function in an Express.js application that allows the server to parse incoming JSON request bodies.
+//     app.use(): This function is used to apply middleware to your Express app.
+//     express.json(): This built-in middleware parses incoming requests with JSON payloads, making the parsed data available in req.body.
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use("/api/products", productRoutes);
+
+app.get("/", (req, res) => {
+  res.send("Hello from node API server");
+});
+
+mongoose
+  .connect(
+    "mongodb+srv://vikasdev:6I9vgZi36vSA5k4Y@nodedb.occy2.mongodb.net/Node-API?retryWrites=true&w=majority&appName=nodeDB"
+  )
+  .then(() => {
+    console.log("connected to database");
+    app.listen(3000, () => {
+      console.log("server is running on port 3000");
+    });
+  })
+  .catch(() => {
+    console.log("Database connection failed!");
+  });
---
+++ routes/product.route.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const router = express.Router();
+const {
+  getProducts,
+  getProductById,
+  addProduct,
+  updateProduct,
+  deleteProduct,
+} = require("../controller/product.controller.js");
+
+router.get("/", getProducts);
+router.get("/:id", getProductById);
+router.post("/", addProduct);
+router.put("/:id", updateProduct);
+router.delete("/:id", deleteProduct);
+
+module.exports = router;
