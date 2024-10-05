be5215fe4797c631f3134accedb58661efbb2695
vikasdev66 <vikasmishra.webdev@gmail.com>
2024-10-05 13:53:01 +0530

    updating env variables

--- index.js
+++ index.js
@@ -3,7 +3,11 @@ const mongoose = require("mongoose");
 const Product = require("./models/product.model.js");
 const productRoutes = require("./routes/product.route.js");
 const cors = require("cors");
+const dotenv = require("dotenv");
 const app = express();
+
+// Load environment variables from .env
+dotenv.config();
 
 // app.use(express.json()); is a middleware function in an Express.js application that allows the server to parse incoming JSON request bodies.
 //     app.use(): This function is used to apply middleware to your Express app.
@@ -19,16 +23,16 @@ app.get("/", (req, res) => {
 
 mongoose
   .connect(
-    "mongodb+srv://vikasdev:6I9vgZi36vSA5k4Y@nodedb.occy2.mongodb.net/Node-API?retryWrites=true&w=majority&appName=nodeDB"
+    `mongodb+srv://${process.env.USERNAMEDB}:${process.env.PASSWORDDB}@nodedb.occy2.mongodb.net/Node-API?retryWrites=true&w=majority&appName=nodeDB`
   )
   .then(() => {
     console.log("connected to database");
-    app.listen(5000, () => {
+    app.listen(process.env.LOCAL_PORT, () => {
       console.log("server is running on port 5000");
     });
   })
-  .catch(() => {
-    console.log("Database connection failed!");
+  .catch((error) => {
+    console.log("Database connection failed!", error);
   });
 
 module.exports = app;
